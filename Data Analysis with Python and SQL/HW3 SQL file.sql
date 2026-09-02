e3d3d3cf2ffad8db04adb2c185ca7a95a31ba11a
James Alex Caramanico <160052029+AlexCaraman@users.noreply.github.com>
2026-09-02 14:35:20 -0400

    work upload

---
+++ Data Analysis with Python and SQL/HW3 SQL file.sql
@@ -0,0 +1,23 @@
+/* CREATE TABLE md_od_2019 (w_geocode DOUBLE PRECISION,h_geocode DOUBLE PRECISION,
+	s000 INT,sa01 INT,sa02 INT,sa03 INT,se01 INT,se02 INT,se03 INT,si01 INT,si02 INT,si03 INT,
+	createdate INT); */
+
+/* CREATE TABLE md_crosswalk (tabblk2010 VARCHAR(15),st VARCHAR(2),stusps VARCHAR(2),stname VARCHAR(100),
+	cty VARCHAR(5), ctyname VARCHAR(100),trct VARCHAR(11),trctname VARCHAR(100),bgrp VARCHAR(12),
+	bgrpname VARCHAR(100),cbsa VARCHAR(5),cbsaname VARCHAR(100),zcta VARCHAR(5),zctaname VARCHAR(100),
+	stpic VARCHAR(7),stpicname VARCHAR(100),ctycsub VARCHAR(10),ctycsubname VARCHAR(100),
+	stcd115 VARCHAR(4),stcd115name VARCHAR(100),stsldl VARCHAR(5),stsldlname VARCHAR(100),stsldu VARCHAR(5),
+	stslduname VARCHAR(100),stschool VARCHAR(7),stschoolname VARCHAR(100),stsecon VARCHAR(7),
+	stseconname VARCHAR(100),trib VARCHAR(5),tribname VARCHAR(100),tsub VARCHAR(7),
+	tsubname VARCHAR(100),stanrc VARCHAR(7),stanrcname VARCHAR(100),necta VARCHAR(5),nectaname VARCHAR(100),
+	mil VARCHAR(22),milname VARCHAR(100),stwib VARCHAR(8),stwibname VARCHAR(100),
+	blklatdd FLOAT,blklondd FLOAT,createdate INT); */
+SELECT ca01 FROM public.md_rac_2019 LIMIT 10;
+SELECT * from public.md_wac_2019 limit 10;
+SELECT * from public.md_od_2019 limit 20;
+SELECT * from public.md_crosswalk limit 30;
+SELECT COUNT (distinct cbsa) from public.md_crosswalk limit 100;
+
+ALTER TABLE public.md_od_2019 ADD combo DOUBLE PRECISION;
+SELECT * from public.md_od_2019 limit 5;
+UPDATE public.md_od_2019 SET combo = w_geocode + h_geocode;
